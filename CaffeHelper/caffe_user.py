fec6c187c0c5b248ebf8aa4d381373c17e792b5c
dnvtmf <dnvtmf@gmail.com>
2017-07-13 22:13:12 +0800

    ...

--- CaffeHelper/caffe_user.py
+++ CaffeHelper/caffe_user.py
@@ -3,6 +3,6 @@ from caffe_layer import *
 
 def BN(data_in, name="BatchNorm"):
     with NameScope(name):
-        bn = BatchNorm(data_in, eps=1e-8)
+        bn = BatchNorm(data_in)
         scale = Scale(bn, bias_term=True)
     return scale
--- CaffeHelper/mnist_MLP.py
+++ CaffeHelper/mnist_MLP.py
@@ -14,18 +14,19 @@ Data([], phase=TEST, source="../mnist_test_lmdb", batch_size=100, backend=Net.LM
 data = [data]
 label = [label]
 out = BN(data, name='bn0')
-fc1 = BinFC(out, name='fc1', num_output=2048, weight_filler=filler_xavier)
-bn1 = BN(fc1, name='bn1')
-relu1 = ReLU(bn1, name='relu1')
-fc2 = FC(relu1, name='fc2', num_output=10, weight_filler=filler_xavier)
-accuracy = Accuracy(fc2 + label)
-loss = HingeLoss(fc2 + label, norm=2)
+out = BinFC(out, name='fc1', num_output=2048, weight_filler=filler_xavier)
+out = BN(out, name='bn1')
+out = ReLU(out, name='relu1')
+out = BN(out, name='bn2')
+out = BinFC(out, name='fc2', num_output=10, weight_filler=filler_xavier)
+accuracy = Accuracy(out + label)
+loss = HingeLoss(out + label, norm=2)
 # loss = SoftmaxWithLoss(ip2 + [label])
 
 # ---------- solver ----
 solver = Solver().net('./model.prototxt').CPU()
 solver.test(test_iter=100, test_interval=500, test_initialization=False)
-solver.train(base_lr=1e-3, lr_policy='step', stepsize=500, gamma=0.1, weight_decay=1e-4, max_iter=1000)
+solver.train(base_lr=1e-3, lr_policy='step', stepsize=100, gamma=0.1, weight_decay=1e-4, max_iter=4000)
 solver.optimizer(type='Adam', momentum=0.9, momentum2=0.999)
 solver.display(display=100)
 solver.snapshot(snapshot=5000, snapshot_prefix='binary_lenet')
